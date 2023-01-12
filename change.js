66c42c065eba04603e5800986433ad187c52575d
niyukurialainnoel <122260450+niyukurialainnoel@users.noreply.github.com>
2023-01-12 16:40:32 +0200

    Add files via upload

---
+++ change.js
@@ -0,0 +1,45 @@
+function burundi(){
+	var image=document.getElementById("image");
+	if (image.src.match("burundi")) {
+		image.src="burundi.png";
+	}else{
+		image.src="burundi.png";
+	}
+}
+
+function kenya(){
+	var image=document.getElementById("image");
+	if (image.src.match("kenya")) {
+		image.src="kenya.png";
+	}else{
+		image.src="kenya.png";
+	}
+}
+
+function tanzania(){
+	var image=document.getElementById("image");
+	if (image.src.match("tz")) {
+		image.src="tz.png";
+	}else{
+		image.src="tz.png";
+	}
+}
+
+function nigeria(){
+	var image=document.getElementById("image");
+	if (image.src.match("naija")) {
+		image.src="naija.png";
+	}else{
+		image.src="naija.png";
+	}
+}
+
+function uganda(){
+	var image=document.getElementById("image");
+	if (image.src.match("ug")) {
+		image.src="ug.png";
+	}else{
+		image.src="ug.png";
+	}
+}
+
